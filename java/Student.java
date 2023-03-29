25762a8bdc3faaa87732341e67ed565764fa65b1
NURFARHANAH03 <126445460+NURFARHANAH03@users.noreply.github.com>
2023-03-30 01:06:57 +0800

    Create Student.java

---
+++ java/Student.java
@@ -0,0 +1,30 @@
+ public class Student {
+        private String username;
+        private String ID;
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getID() {
+            return ID;
+        }
+
+        public void setID(String ID) {
+            this.ID = ID;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
