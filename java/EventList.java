ec1fc9d6733b7adf0181855899d210be84ce3dbc
NURFARHANAH03 <126445460+NURFARHANAH03@users.noreply.github.com>
2023-04-01 18:28:26 +0800

    Create EventList.java

---
+++ java/EventList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class EventList {
+    public ArrayList<Event> events = new ArrayList<>();
+    public void addEvent(Event event){
+        events.add(event);
+    }
+}
+
