3f97a50ab1d2acc5fa12997275c79247c0619a5d
NURFARHANAH03 <126445460+NURFARHANAH03@users.noreply.github.com>
2023-04-02 02:19:51 +0800

    Update TaskList.java

--- java/TaskList.java
+++ java/TaskList.java
@@ -1,8 +1,13 @@
 import java.util.ArrayList;
-
 public class TaskList {
     public ArrayList<Task> tasks = new ArrayList<>();
     public void addTask(Task task){
         tasks.add(task);
     }
+    public void removeTask(Task task){
+        tasks.remove(task);
+    }
+
+
+
 }
