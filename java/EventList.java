7609925dae8d45bad02b57ed2fec412b6e359cfc
NURFARHANAH03 <126445460+NURFARHANAH03@users.noreply.github.com>
2023-04-02 02:21:58 +0800

    Update EventList.java

--- java/EventList.java
+++ java/EventList.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
-
 public class EventList {
     public ArrayList<Event> events = new ArrayList<>();
     public void addEvent(Event event){
         events.add(event);
     }
+    public void removeEvent(Event event){
+        events.remove(event);
+    }
 }
+
+
 
