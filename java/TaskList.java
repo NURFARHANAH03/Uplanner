a465bbfd57f76c7e58480d55dc9e6980f8e8e3e5
NURFARHANAH03 <126445460+NURFARHANAH03@users.noreply.github.com>
2023-04-01 18:26:19 +0800

    Create TaskList.java

---
+++ java/TaskList.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    public ArrayList<Task> tasks = new ArrayList<>();
+    public void addTask(Task task){
+        tasks.add(task);
+    }
+}
