8b66aca467311f6a0761ed4380d7ebb1f76a011d
wiyaa13 <126445322+wiyaa13@users.noreply.github.com>
2023-04-02 22:39:26 +0800

    Update MainUpdate.java

--- java/MainUpdate.java
+++ java/MainUpdate.java
@@ -158,6 +158,12 @@ public class Main {
         //*Settings*
 
         //*Reminder
+        System.out.println("Reminder");
+        Reminder1 rmd1 = new Reminder1();
+          rmd1.DisplayReminder1();
+
+        Reminder2 rmd2 = new Reminder2();
+          rmd2.DisplayReminder2();
     }
 
 
