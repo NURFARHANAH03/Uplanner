83b4662cd2803e2f90bad633a46b0a6a04436b5c
wiyaa13 <126445322+wiyaa13@users.noreply.github.com>
2023-04-02 21:45:19 +0800

    Update Reminder1.java

--- java/Reminder1.java
+++ java/Reminder1.java
@@ -3,10 +3,13 @@ public class Reminder1 extends Date {
     public static void main (String[] args){
         Reminder1 rmd1 = new Reminder1();
         rmd1.get_start_time();
-        rmd1.get_start_time();
+        rmd1.get_end_time();
+
+
     }
-    public void DisplayReminder1() {
-        System.out.println(get_start_time());
-        System.out.println(get_end_time());
+        public void DisplayReminder1(){
+
+            System.out.println("Time start:\t"+get_start_time());
+            System.out.println("Time End:\t"+get_end_time());
     }
 }
